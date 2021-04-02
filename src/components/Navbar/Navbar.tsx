e6b4bcbbe53f3f79a74dead628ecdc657f48f209
MaxFas <FastsovMax@yandex.ru>
2021-04-02 12:33:20 +0300

    create class component Users (52-53)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,6 +7,7 @@ function Navbar() {
         <nav className={classes.nav}>
             <div className={classes.item}><NavLink to={'/profile'} activeClassName={classes.active}>Profile</NavLink></div>
             <div className={classes.item}><NavLink to={'/dialogs'} activeClassName={classes.active}>Messages</NavLink></div>
+            <div className={classes.item}><NavLink to={'/users'} activeClassName={classes.active}>Users</NavLink></div>
             <div className={classes.item}><NavLink to={'/News'} activeClassName={classes.active}>News</NavLink></div>
             <div className={classes.item}><NavLink to={'/Music'} activeClassName={classes.active}>Music</NavLink></div>
             <div className={classes.item}><NavLink to={'/Settings'} activeClassName={classes.active}>Settings</NavLink></div>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,35 +12,30 @@ export type UsersPageType = {
     setUsers: (users: Array<UserType>) => void
 }
 
-export const Users = (props: UsersPageType) => {
-
-    if (props.users.length === 0){
+export class Users extends React.Component<UsersPageType>{
+    constructor(props: UsersPageType) {
+        super(props);
         axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => props.setUsers(response.data.items))
     }
 
-    return (
-        <div>
-            {props.users.map(u => <div key={u.id}>
+        render () {
+            return (
+                <div>
+                    {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div><img className={s.userPhoto} src={u.photos.small !== null? u.photos.small: userPhoto} alt=""/></div>
                     <div>
-                        {u.followed? <button onClick={()=> props.unFollow(u.id)}>UnFollow</button>:
-                            <button onClick={()=> props.follow(u.id)}> Follow</button>}
+                        {u.followed? <button onClick={()=> this.props.unFollow(u.id)}>UnFollow</button>:
+                            <button onClick={()=> this.props.follow(u.id)}> Follow</button>}
                     </div>
                 </span>
-                <span>
+                        <span>
                     <span><div>{u.name}</div><div>{u.status}</div></span>
                      <span><div></div><div></div></span>
                 </span>
-            </div>)}
-        </div>
-    )
+                    </div>)}
+                </div>)
+        }
 }
-
-
-// props.setUsers([{id: 1, photoUrl:'https://igrasan.ru/wp-content/uploads/2014/05/zadrot.jpg',followed: true,  fullName: 'MaxF', status: 'WHAT THE FUCK IS GOING ON??', location: {city: 'Petrozavodsk', country: 'Russia'}},
-//     {id: 2, photoUrl:'https://igrasan.ru/wp-content/uploads/2014/05/zadrot.jpg', followed: false,  fullName: 'BillG', status: 'Check my new startup', location: {city: 'Petrozavodsk', country: 'Russia'}},
-//     {id: 3, photoUrl:'https://igrasan.ru/wp-content/uploads/2014/05/zadrot.jpg',followed: true,  fullName: 'JenyaP', status: 'Working again!!', location: {city: 'Petrozavodsk', country: 'Russia'}},
-//     {id: 4, photoUrl:'https://igrasan.ru/wp-content/uploads/2014/05/zadrot.jpg',followed: true,  fullName: 'StasP', status: 'I am a lier', location: {city: 'Petrozavodsk', country: 'Russia'}}])
